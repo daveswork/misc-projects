a7ac1c195fca3b8e5af8e6495b52b2db0b13e407
Dave J <daveswork@users.noreply.github.com>
2018-02-27 02:02:40 -0500

    Added a short script to help me generate a bunch of empty directories for when I take Udemy courses.

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 
-#Misc scripts that have helped me with some chores.
+# Misc scripts that have helped me with some chores.
 
 
 
-Python 2.7
+## Python 2.7
 
 
-flat-file-parser.py this takes a file with fixed length fields and converts it
+- flat-file-parser.py this takes a file with fixed length fields and converts it
 into a csv file. Super handy when there are over 100 fields to split. 
+
+- course-directories.py this generates a bunch of empty directories for me to work in. 
+Mostly useful when I take a Udemy course and want to keep work for each section in their directory.
 
---
+++ course-directories/course-directories.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+def create_dirs(sections, base):
+    for i in range(1, sections+1):
+        suffix = ""
+        if len(str(i))<2:
+            suffix = "0"+str(i)
+        else:
+            suffix = str(i)
+        os.makedirs(base+"/section-"+suffix)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("sections", help="The number of section directories to create", type=int)
+parser.add_argument("base", nargs = '?', default=os.getcwd(), help="The directory to start from.", type=str)
+args = parser.parse_args()
+
+if args.sections:
+    create_dirs(args.sections, args.base)
